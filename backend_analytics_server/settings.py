f63606f1b3689c06c23c3f455353e4c09c2d9ec4
andrsbv <152298255+andrsbv@users.noreply.github.com>
2025-08-20 09:58:21 -0500

    database cambio

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -92,13 +92,14 @@ WSGI_APPLICATION = 'backend_analytics_server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQLDATABASE', 'security'),
-        'USER': os.environ.get('MYSQLUSER', 'root'),
-        'PASSWORD': os.environ.get('MYSQLPASSWORD', ''),
-        'HOST': os.environ.get('MYSQLHOST', 'localhost'),
-        'PORT': os.environ.get('MYSQLPORT', '3306'),
+        'NAME': os.environ.get('MYSQLDATABASE'),
+        'USER': os.environ.get('MYSQLUSER'),
+        'PASSWORD': os.environ.get('MYSQLPASSWORD'),
+        'HOST': os.environ.get('MYSQLHOST'),
+        'PORT': os.environ.get('MYSQLPORT'),
     }
 }
+
 
 
 # Password validation
